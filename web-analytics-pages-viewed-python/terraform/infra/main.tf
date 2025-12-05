5e21d82542076f580c55694f82263fa80c2668a7
Aaron Ginder <aaronginder@hotmail.co.uk>
2025-12-05 09:29:34 +0000

    fix: Remove redundant files and add yamllint, precommit and security workflow

--- web-analytics-pages-viewed-python/terraform/infra/main.tf
+++
@@ -1,8 +0,0 @@
-module "pubsub_proc" {
-  count                   = var.project_type == "proc" ? 1 : 0
-  source                  = "moodule"
-  version                 = "v2.0.2"
-  stage                   = var.stage
-  project_id              = var.project_id
-  pubsub_yaml_config_path = var.proc_yaml_config_path
-}
--- web-analytics-pages-viewed-python/terraform/variables.tfvars
+++
@@ -1,2 +0,0 @@
-data_yaml_config_path   = "../data_substituted.yaml"
-proc_yaml_config_path   = "../proc_substituted.yaml"
